8b51a62b122b535358d73a4aa564fefc8217e59d
alpturan-dev <abdurrahmanalpturan13@gmail.com>
2025-02-02 22:55:10 +0300

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/v0/b/hendese-otomotiv.appspot.com/o/**',
+        search: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
