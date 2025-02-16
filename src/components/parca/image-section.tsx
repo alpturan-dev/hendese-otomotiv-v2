3f23fae2b78ced2174c84d8ffd8c442c87eb68a6
alpturan-dev <abdurrahmanalpturan13@gmail.com>
2025-02-16 13:55:34 +0300

    add swipe feature to image section

--- src/components/parca/image-section.tsx
+++ src/components/parca/image-section.tsx
@@ -4,6 +4,7 @@ import { ArrowLeft, ArrowRight } from 'lucide-react';
 import Image from 'next/image'
 import React, { useState } from 'react'
 import { twJoin } from 'tailwind-merge';
+import { useSwipeable } from 'react-swipeable';
 
 export default function ImageSection({ images }: { images: Array<string> }) {
     const [imgIndex, setImgIndex] = useState(0);
@@ -15,16 +16,29 @@ export default function ImageSection({ images }: { images: Array<string> }) {
         );
     };
 
+    const handlers = useSwipeable({
+        onSwipedLeft: () => handleImgIndex("right"),
+        onSwipedRight: () => handleImgIndex("left"),
+        preventScrollOnSwipe: true,
+        trackMouse: true,
+    });
+
     return (
         <div id="images" className='pt-4 flex flex-col md:flex-row-reverse gap-4 md:col-span-2'>
-            <div className='h-[350px] md:h-[420px]'>
+            <div className='h-[350px] md:h-[420px]' {...handlers}>
                 <div className="relative flex items-center justify-center select-none h-full">
                     <button className="z-50 cursor-pointer absolute left-0 top-1/2 transform -translate-y-1/2 h-full flex items-center" onClick={() => handleImgIndex("left")}>
                         <div className="ml-2 rounded h-8 w-8 bg-primary/90 flex items-center">
                             <ArrowLeft strokeWidth={4} className="pl-2 text-white" />
                         </div>
                     </button>
-                    <Image src={images[imgIndex]} alt={images[imgIndex]} width={350} height={350} className='border border-primary/20 rounded-lg h-full shadow-lg shadow-primary/30' />
+                    <Image
+                        src={images[imgIndex]}
+                        alt={images[imgIndex]}
+                        width={350}
+                        height={350}
+                        className='border border-primary/20 rounded-lg h-full shadow-lg shadow-primary/30 transition-transform duration-300 ease-in-out'
+                    />
                     <button className="cursor-pointer absolute right-0 top-1/2 transform -translate-y-1/2 h-full flex items-center" onClick={() => handleImgIndex("right")}>
                         <div className="mr-2 rounded h-8 w-8 bg-primary/90 flex items-center">
                             <ArrowRight strokeWidth={4} className="pl-2 text-white" />
@@ -35,12 +49,24 @@ export default function ImageSection({ images }: { images: Array<string> }) {
             <div className='grid grid-cols-4 gap-2 md:grid-cols-1 md:grid-rows-4'>
                 {images.map((img: string, index: number) => {
                     if (index < 4)
-                        return <div key={index} className='h-[80px] md:h-[100px]' onClick={() => setImgIndex(index)}>
-                            <Image src={img} alt={img} width={100} height={100} className={twJoin(
-                                'border rounded-lg h-full',
-                                imgIndex === index && "border-2 border-primary"
-                            )} />
-                        </div>
+                        return (
+                            <div
+                                key={index}
+                                className='h-[80px] md:h-[100px] cursor-pointer'
+                                onClick={() => setImgIndex(index)}
+                            >
+                                <Image
+                                    src={img}
+                                    alt={img}
+                                    width={100}
+                                    height={100}
+                                    className={twJoin(
+                                        'border rounded-lg h-full',
+                                        imgIndex === index && "border-2 border-primary"
+                                    )}
+                                />
+                            </div>
+                        )
                 })}
             </div>
         </div>
